b83c8c8e3a1b6583803ec00f1724cbb6178ec8f2
Evan Bluhm <evan.bluhm@ibm.com>
2018-09-05 21:36:58 -0700

    Rename README

--- filewatcher.sh
+++ filewatcher.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x 
 # 
-# This is just a wrapper for http://entrproject.org/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GPG_TTY=$(tty)
 export GPG_TTY
 cd "$DIR" || exit 1
+
+# un-comment in case of virtualenv
 # source venv/bin/activate
 
 # Can also use `find` here if silversurfer not installed
+# This is just a wrapper for http://entrproject.org/
 # /usr/bin/ag -l | /usr/bin/entr -d bash "$DIR/buildDocs.sh"
 
 pip3 install --user sphinx-autobuild
-sphinx-autobuild "$DIR" "$DIR/_build/html"
+sphinx-autobuild -p 8888 -H "0.0.0.0" -i ".git/*" "$DIR" "$DIR/_build/html"
